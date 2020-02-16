3606478f52856e28e66616d676764ff70e217c54
CaroVisen <carolinaaylen26@gmail.com>
2020-02-16 14:35:24 -0300

    Creo componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './Componentes/home/home.component';
+import { ContactoComponent } from './Componentes/contacto/contacto.component';
+import { CotizacionComponent } from './Componentes/cotizacion/cotizacion.component';
+import { NosotrosComponent } from './Componentes/nosotros/nosotros.component';
+import { ProductosComponent } from './Componentes/productos/productos.component';
+import { ProductoComponent } from './Componentes/producto/producto.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'cotizacion', component: CotizacionComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'productos', component: ProductosComponent },
+  { path: 'producto', component: ProductoComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Componentes/navbar/navbar.component';
+import { HomeComponent } from './Componentes/home/home.component';
+import { ProductosComponent } from './Componentes/productos/productos.component';
+import { ProductoComponent } from './Componentes/producto/producto.component';
+import { CotizacionComponent } from './Componentes/cotizacion/cotizacion.component';
+import { NosotrosComponent } from './Componentes/nosotros/nosotros.component';
+import { ContactoComponent } from './Componentes/contacto/contacto.component';
+import { FooterComponent } from './Componentes/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatMenuModule} from '@angular/material/menu';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProductosComponent,
+    ProductoComponent,
+    CotizacionComponent,
+    NosotrosComponent,
+    ContactoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
